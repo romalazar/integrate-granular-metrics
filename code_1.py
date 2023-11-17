7e15f63ce1cd0c1c2cce347480a4595d801a1975
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-11-18 01:50:28 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,30 @@
-}
-    });
-function generateRandomData() {
-}
-    const data = [];
-        console.log("Random Number: " + item);
+main();
         data.push(Math.floor(Math.random() * 100) + 1);
 function main() {
+        console.log("Random Number: " + item);
 
-    data.forEach(item => {
-    }
-main();
     return data;
-
     const data = generateRandomData();
+function generateRandomData() {
 
     for (let i = 0; i < 10; i++) {
+    const data = [];
+    }
+
+}
+}
+    data.forEach(item => {
+    });    for item in data:
+    main()
+
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+def main():
+
+
+    data = generate_random_data()
+
+if __name__ == "__main__":
